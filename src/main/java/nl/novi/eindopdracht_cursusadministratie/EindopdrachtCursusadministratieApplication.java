eb5e9e7628287452d1ccfdc7bfb414c960024d46
wesleydewinne <wesleydewinne@gmail.com>
2025-10-04 17:08:51 +0200

    first push

---
+++ src/main/java/nl/novi/eindopdracht_cursusadministratie/EindopdrachtCursusadministratieApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht_cursusadministratie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtCursusadministratieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtCursusadministratieApplication.class, args);
+    }
+
+}
