4034a99e6ee977dfae2e8b7ca504e02c4b3f3c44
wesleydewinne <wesleydewinne@gmail.com>
2025-10-08 11:53:17 +0200

    Feat: add @NotNull validation annotations to DTO classes

--- src/main/java/nl/novi/eindopdracht_cursusadministratie/dto/certificate/GenerateCertificateRequest.java
+++ src/main/java/nl/novi/eindopdracht_cursusadministratie/dto/certificate/GenerateCertificateRequest.java
@@ -1,7 +1,13 @@
 package nl.novi.eindopdracht_cursusadministratie.dto.certificate;
 
+import jakarta.validation.constraints.NotNull;
+
 /**
  * Request DTO voor het genereren van een certificaat.
  * Wordt gebruikt door trainers om een certificaat toe te voegen voor een cursist.
  */
-public record GenerateCertificateRequest(Long courseId, Long studentId, String issuedBy) {}
+public record GenerateCertificateRequest(
+       @NotNull Long courseId,
+       @NotNull Long studentId,
+       @NotNull String issuedBy
+) {}
