4c8ace75471e8776dd569d3ca0b18a08dad649a3
wesleydewinne <wesleydewinne@gmail.com>
2025-10-11 16:39:55 +0200

    Update application.properties and refresh database data.sql

---
+++ src/main/java/nl/novi/eindopdracht_cursusadministratie/exception/CapacityExceededException.java
@@ -0,0 +1,7 @@
+package nl.novi.eindopdracht_cursusadministratie.exception;
+
+public class CapacityExceededException extends RuntimeException {
+  public CapacityExceededException(String message) {
+    super(message);
+  }
+}
